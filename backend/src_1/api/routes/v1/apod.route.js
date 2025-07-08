e9f342dcdbcdc772109da71bd20c14b0a6e41bd6
awais6667A <awais.karamat@arhamsoft.org>
2025-07-08 16:10:23 +0500

    added read me file

--- backend/src_1/api/routes/v1/apod.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const controller = require('../../controllers/apod.controller');
-const router = express.Router();
-
-router.route('/').get(controller.List)
-
-module.exports = router;
--- backend/src_1/api/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const apod = require('./apod.route');
-
-const router = express.Router();
-
-router.use('/apod', apod);
-
-module.exports = router;
